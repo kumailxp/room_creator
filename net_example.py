d011a5b850800723f4147b257fab97adfb036443
kumail ahmed <kumail89@gmail.com>
2022-06-19 23:57:15 +0200

    generate gtaph and imporve logic

--- net_example.py
+++ net_example.py
@@ -1,6 +1,50 @@
 #!/bin/python
 
-import networkx
+#import networkx
+
+
+# First start xserver on Windows (WSL settings)
+# ./vcxsrv.exe :0 -multiwindow -clipboard -wgl -ac
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.use('TkAgg')
+
+
+#subplot(r,c) provide the no. of rows and columns
+f, ax = plt.subplots(2,1)
+
+a_1 = [
+        [[43, 120, 227], [66, 245, 78], [66, 245, 78]],
+        [[43, 120, 227], [245, 84, 66], [66, 245, 78]],
+        [[43, 120, 227], [66, 245, 78], [66, 245, 78]]
+      ]
+a_2 = [[[43, 120, 227], [66, 245, 78], [66, 245, 78]], [[43, 120, 227], [245, 84, 66], [43, 120, 227]], [[43, 120, 227], [43, 120, 227], [43, 120, 227]]]
+
+# use the created array to output your multiple images. In this case I have stacked 4 images vertically
+ax[0].imshow(a_1)
+ax[1].imshow(a_2)
+
+# t = np.arange(0.0, 2.0, 0.01)
+# s = 1 + np.sin(2*np.pi*t)
+# plt.plot(t, s)
+
+# plt.title('About as simple as it gets, folks')
+plt.show()
+
+exit(0)
+
+l = [1,2,3,4,5,6,7]
+def get_inner_paths(path_list):
+    r = []
+    for i in range(1, len(path_list)-1):
+        r.append(path_list[i:])
+    return r
+
+print(get_inner_paths(l))
+
 
 # G : networkx.Graph = networkx.complete_graph(3)
 # print(G.size())
@@ -10,18 +54,18 @@ import networkx
 # print(G.edges())
 
 
-edges = [(0, 1), (0, 3), (3, 4), (3, 6), (6, 7), (1, 2), (1, 4), (4, 5), (4, 7), (7, 8), (2, 5), (5, 8)]
+# edges = [(0, 1), (0, 3), (3, 4), (3, 6), (6, 7), (1, 2), (1, 4), (4, 5), (4, 7), (7, 8), (2, 5), (5, 8)]
 
-G : networkx.Graph = networkx.Graph()
-G.add_edges_from(edges)
-print(G.size())
-print(G.nodes())
-print("all paths")
-for path in networkx.all_simple_paths(G, source=0, target=1):
-    print(path)
+# G : networkx.Graph = networkx.Graph()
+# G.add_edges_from(edges)
+# print(G.size())
+# print(G.nodes())
+# print("all paths")
+# for path in networkx.all_simple_paths(G, source=0, target=1):
+#     print(path)
 
-G.remove_node(0)
-for path in networkx.all_simple_paths(G, source=2, target=1):
-    print(path)
-for path in networkx.all_simple_paths(G, source=4, target=1):
-    print(path)
+# G.remove_node(0)
+# for path in networkx.all_simple_paths(G, source=2, target=1):
+#     print(path)
+# for path in networkx.all_simple_paths(G, source=4, target=1):
+#     print(path)
--- paths.py
+++ paths.py
@@ -1,15 +1,30 @@
 #!/bin/python
-from audioop import tomono
+import time
+from typing import List
 import networkx
+import matplotlib
 import matplotlib.pyplot as plt
 import math
 from colored import fg, bg, attr
+from numpy import array, block
+
+matplotlib.use('TkAgg')
 
 class BlockType:
     Core = 1
     Green = 2
     Blue = 4
     Empty = 15
+    Yellow = 3
+    Gray = 8
+
+class RGB:
+    Red = [245, 84, 66]
+    Green = [66, 245, 78]
+    Blue = [43, 120, 227]
+    White = [255, 255, 255]
+    Yellow = [245, 237, 12]
+    Gray = [112, 112, 109]
 
 
 class Coordinate:
@@ -32,6 +47,33 @@ class Coordinate:
         outside_map = (-1,-1)
         inside_map = 1
 
+    def get_rgb_array(self):
+        rgb_array = self.coordinate_map
+        color = RGB.White
+        for _,coordinate in self.node_map.items():
+            x, y = coordinate
+            if self.coordinate_map[x][y] == BlockType.Core:
+                color = RGB.Red
+            elif self.coordinate_map[x][y] == BlockType.Green:
+                color = RGB.Green
+            elif self.coordinate_map[x][y] == BlockType.Blue:
+                color = RGB.Blue
+            elif self.coordinate_map[x][y] == BlockType.Empty:
+                color = RGB.White
+            elif self.coordinate_map[x][y] == BlockType.Yellow:
+                color = RGB.Yellow
+            elif self.coordinate_map[x][y] == BlockType.Gray:
+                color = RGB.Gray
+            rgb_array[x][y] = color
+        return rgb_array
+
+
+    def get_empty_rgb(self):
+        rgb_array = self.coordinate_map
+        for _,coordinate in self.node_map.items():
+            x, y = coordinate
+            rgb_array[x][y] = RGB.White
+        return rgb_array
 
     def clean_map(self):
         self.coordinate_map = [ [BlockType.Empty] * self.get_num_rows() for i in range(0, self.get_num_cols())]
@@ -159,78 +201,148 @@ def print_layouts(layout):
     print(len(layout))
     for room in layout:
         print(room)
-    # for i in range(0, 3):
-    #     room_0 = layout[i]
-    #     room_1 = layout[i+1]
-    #     room_2 = layout[i+2]
 
-    #     for col in range(0, 3):
-    #         print(f"{room_0.get_row_info_as_color(col)}*{room_1.get_row_info_as_color(col)}*{room_2.get_row_info_as_color(col)}", end='')
-        
-    #     print("\n")
+def get_inner_paths(path_list):
+    r = []
+    for i in range(1, len(path_list)-1):
+        r.append(path_list[i:])
+    return r
+
+
+def create_other_area(all_nodes, room_map, area_for_room_a):
+    area_for_room_b = list(set(all_nodes) - set(area_for_room_a))
+    if room_map.core_block_node in area_for_room_b:
+        area_for_room_b.remove(room_map.core_block_node)
+    return area_for_room_b
+
+cached_room_a = []
+def create_inner_rooms(all_nodes, room_map, core_connected_blocks, area_for_room_a, source_node):
+    room_map_graph: networkx.Graph = room_map.convert_to_graph()
+    inner_paths = get_inner_paths(area_for_room_a)
+    all_inner_rooms = []
+    for p in inner_paths:
+
+        # if p in cached_room_a:
+        #     break
+        # cached_room_a.append(p)
+        area_of_inner_room = create_other_area(all_nodes, room_map, p)
+        if do_blocks_meet_criteria(room_map_graph, area_of_inner_room, core_connected_blocks):
+            room_plan = create_room(all_nodes, room_map, p, area_of_inner_room, source_node)
+            all_inner_rooms.append(room_plan)
+
+    return all_inner_rooms
+
+
+def create_room(all_nodes, room_map, area_for_room_a, area_for_room_b, source_node = -1):
+    #print("A:", area_for_room_a, " B: ", area_for_room_b)
+    new_room = Coordinate(room_map.rows, room_map.cols, room_map.core_block)
+
+    for node in area_for_room_a:
+        x, y = new_room.node_map[node]
+        new_room.set_value(x, y, BlockType.Green)
+    for node in area_for_room_b:
+        x, y = new_room.node_map[node]
+        new_room.set_value(x, y, BlockType.Blue)
+
+    x, y = new_room.node_map[area_for_room_a[-1]]
+    new_room.set_value(x, y, BlockType.Core)
+
+    if source_node != -1:
+        sx, sy = new_room.node_map[source_node]
+        new_room.set_value(sx, sy, BlockType.Yellow)
+
+    return new_room
+
+def do_blocks_meet_criteria(room_map_graph, area_for_room_b, core_connected_blocks):
+    room_b_graph: networkx.Graph =room_map_graph.subgraph(area_for_room_b)
+
+    r = set(area_for_room_b).intersection(set(core_connected_blocks.keys()))
+    if len(r) == 0:
+        return False
+
+    if not networkx.is_connected(room_b_graph):
+        return False
+
+    return True
+
+
+def get_room_plan(all_nodes, core_connected_blocks, room_map, area_for_room_a, source_node):
+    room_map_graph: networkx.Graph = room_map.convert_to_graph()
+    area_for_room_b = create_other_area(all_nodes, room_map, area_for_room_a)
+
+    all_rooms = []
+    if do_blocks_meet_criteria(room_map_graph, area_for_room_b, core_connected_blocks):
+        room_plan = create_room(all_nodes, room_map, area_for_room_a, area_for_room_b, source_node)
+        all_rooms.append(room_plan)
+
+    inner_rooms = create_inner_rooms(all_nodes, room_map, core_connected_blocks, area_for_room_a, source_node)
+    for r in inner_rooms:
+        all_rooms.append(r)
+
+    return all_rooms
+
+
+def get_subplot_layout(all_rgb_arrays):
+
+    array_len = len(all_rgb_arrays)
+    rows = int(math.sqrt(array_len))
+    extra_col = 0 if array_len%rows  == 0 else 1
+    cols = extra_col + int(array_len/rows)
+    return rows, cols
+
+
+def plot_on_graph(room_map, all_rgb_arrays):
+    print("subplot layout:", get_subplot_layout(all_rgb_arrays))
+    print("num layouts", len(all_rgb_arrays))
+    subplot_row, subplot_col = get_subplot_layout(all_rgb_arrays)
+    _, ax = plt.subplots(subplot_col, subplot_row)
+    id = 0
+    rgb_array_len = len(all_rgb_arrays)
+    for c in range(0, subplot_col):
+        for r in range(0, subplot_row):
+            if id > rgb_array_len -1:
+                ax[c][r].imshow(room_map.get_empty_rgb())
+                ax[c][r].set_axis_off()
+            else:
+                ax[c][r].imshow(all_rgb_arrays[id])
+                ax[c][r].set_axis_off()
+            id += 1
+
+    plt.axis('off')
+
+
+def get_paths_from_source(source_node, room_map, room_graph, all_nodes, core_connected_blocks, return_as_rgb = True):
+    cutoff = (int(room_map.cols*room_map.rows)/2) + 1
+    result = []
+    for path in networkx.all_simple_paths(room_graph, source=source_node, target=room_map.core_block_node, cutoff=cutoff):
+        current_room_plan: List[Coordinate] = get_room_plan(all_nodes, core_connected_blocks, room_map, path, -1)
+        if len(current_room_plan) != 0:
+                for cp in current_room_plan:
+                    if return_as_rgb:
+                        rgb_box = cp.get_rgb_array()
+                        if not rgb_box in result:
+                            result.append(rgb_box)
+                    else:
+                        if not cp in result:
+                            result.append(cp)
+    return result
 
 def main():
-    room_map = Coordinate(3,3, (1,2))
-    # print(room_map.set_value(1, 0, BlockType.Core))
-    # print(room_map)
-
+    room_map = Coordinate(5,5, (2,2))
     room_graph: networkx.Graph = room_map.convert_to_graph()
-    # print(room_graph.size())
-    all_nodes = room_graph.nodes()
+#    all_nodes = room_graph.nodes()
     core_connected_blocks = room_graph[room_map.core_block_node]
     print("core_connected_blocks", list(core_connected_blocks.keys()))
-    # networkx.draw_networkx(room_graph, with_labels=True)
-    # plt.show()
     print(room_map.node_map)
-    #print(list(networkx.bfs_tree(room_graph, 5).edges()))
-    all_layouts = []
-    tmp_b_rooms = []
-    new_room = Coordinate(room_map.rows, room_map.cols, room_map.core_block)
-    new_room_graph: networkx.Graph = room_map.convert_to_graph()
-    for path in networkx.all_simple_paths(room_graph, source=0, target=room_map.core_block_node):
-        new_room.clean_map()
-        area_for_room_a = path
-        area_for_room_b = list(set(all_nodes) - set(area_for_room_a))
-        if room_map.core_block_node in area_for_room_b:
-            area_for_room_b.remove(room_map.core_block_node)
-        room_b_graph: networkx.Graph =new_room_graph.subgraph(area_for_room_b)
-        # room_b_graph.add_node(room_map.core_block_node)
-        #room_a_graph: networkx.Graph =new_room_graph.subgraph(area_for_room_a)
-        r = set(area_for_room_b).intersection(set(core_connected_blocks.keys()))
-        if len(r) == 0:
-            continue
 
-        if not networkx.is_connected(room_b_graph):
-            continue
+    for source_node in [4]:
+        #cached_room_a.clear()
+        all_nodes = room_graph.nodes()
+        #source_node = 0
+        all_rgb_arrays = get_paths_from_source(source_node, room_map, room_graph, all_nodes, core_connected_blocks)
+        plot_on_graph(room_map, all_rgb_arrays)
 
-
-        room_b_is_there = False
-        for x in tmp_b_rooms:
-            #print("x ", x, " area_for_room_b ", area_for_room_b)
-            if area_for_room_b == x:
-                room_b_is_there = True
-                break
-        if room_b_is_there:
-            continue
-        print("A:", area_for_room_a, " B: ", area_for_room_b)
-        tmp_b_rooms.append(area_for_room_b)
-       
-        #print(networkx.is_connected(room_a_graph))
-        for node in area_for_room_a:
-            x, y = new_room.node_map[node]
-            new_room.set_value(x, y, BlockType.Green)
-        for node in area_for_room_b:
-            x, y = new_room.node_map[node]
-            new_room.set_value(x, y, BlockType.Blue)
-        
-        x, y = new_room.node_map[area_for_room_a[-1]]
-        new_room.set_value(x, y, BlockType.Core)
-        all_layouts.append(new_room)
-        print(new_room)
-
-    print("num layouts", len(all_layouts))
-    #print_layouts(all_layouts)
-    #print(room_graph[1])
+    plt.show()
 
 
 if __name__ == "__main__":
